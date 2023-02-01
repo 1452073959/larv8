2c5d4bd35664f41a2c86832c8b18838cb2153953
zcc <1452073959@qq.com>
2023-02-01 17:51:48 +0800

    1

--- app/Http/Controllers/Web/Testcontroller.php
+++ app/Http/Controllers/Web/Testcontroller.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers\Web;
 
 use App\Events\TestEvent;
 use App\Http\Controllers\Controller;
+use App\Jobs\SendNotice;
 use App\Listeners\TestListener;
+use App\Models\TdDeal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Testcontroller extends Controller
 {
     //
     public function index()
     {
-        $a= event(new TestEvent(['da'=>1222]));
-        echo 123111;
+
+        $a=  TdDeal::where('send_status','1')->limit(100)
+            ->orderby('id',"desc")->get()->toarray();
+//        dd($a);die;
+        foreach ($a as $k1=>$v1)
+        {
+            SendNotice::dispatch($v1);
+        }
+
     }
+
+
 }
--- app/Jobs/SendNotice.php
+++ app/Jobs/SendNotice.php
@@ -10,10 +10,12 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Models\TdDeal;
+
 class SendNotice implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $url='http://49.235.76.14:687';
     /**
      * 任务可尝试次数.
      *
@@ -28,16 +30,17 @@ class SendNotice implements ShouldQueue
     public $failOnTimeout = true;
 
     public $data;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(TdDeal $data)
+    public function __construct($data)
     {
         //
         $this->data = $data;
-//        dump( $this->data->toArray());
+//        dump( $this->data);die;
     }
 
     /**
@@ -45,12 +48,82 @@ class SendNotice implements ShouldQueue
      *
      * @return void
      */
-    public function handle(TdDeal $data)
+    public function handle()
     {
-        //
-                dump( $this->data->toArray());
-        $response= http_post_data('http://la.test/api/event',$data);
 
-        Log::alert("测试".json_encode($response,true));
+//
+//        if($res['1']['ret_code']!="0000"){
+//            $this->fail();
+//        }else{
+//            dump($res);
+//        }
+
+//            log::write($res);
+
+        //
+        dump($this->data['brand_id']);
+        $status= TdDeal::find($this->data['id']);
+        if ($this->data['brand_id'] == "联动") {
+//            dump( json_decode($this->data['Raw_data'],true));die;
+            if ($this->data['agent_no'] == 8600083) {
+                $res = http_post_data($this->url."/notice/Liandong/transOrderNofity", json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600118) {
+                $res = http_post_data($this->url."/notice/Liandong2/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600123) {
+                $res = http_post_data($this->url."/notice/Liandong3/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600137) {
+                $res = http_post_data($this->url."/notice/Liandong4/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600139) {
+                $res = http_post_data($this->url."/notice/Liandong5/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600157) {
+                $res = http_post_data($this->url."/notice/Liandong6/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600158) {
+                $res = http_post_data($this->url."/notice/Liandong7/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600167) {
+                $res = http_post_data($this->url."/notice/Liandong8/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600169) {
+                $res = http_post_data($this->url."/notice/Liandong9/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600180) {
+                $res = http_post_data($this->url."/notice/Liandong10/transOrderNofity", json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600190) {
+                $res = http_post_data($this->url."/notice/Liandong11/transOrderNofity", json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600210) {
+                $res = http_post_data($this->url."/notice/Liandong12/transOrderNofity", json_decode($this->data['Raw_data'],true));
+            } elseif ($this->data['agent_no'] == 8600214) {
+                $res = http_post_data($this->url."/notice/Liandong13/transOrderNofity",json_decode($this->data['Raw_data'],true));
+            }
+        }elseif($this->data['brand_id'] == "电银"){
+            $res = http_post_data($this->url."/notice/dianyin/index", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "收付贝"){
+            $res = http_post_data($this->url."/notice/shoufubei/payment", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "钱宝"){
+            $res = http_post_data($this->url."/notice/qianbao/index", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "合利宝"){
+            $res = http_post_data($this->url."/notice/helibao/payment", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "金控"){
+            $res = http_post_data($this->url."/notice/Jinkong/transOrderNofity", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "海科"){
+            $res = http_post_data($this->url."/notice/Haike/trade", json_decode($this->data['Raw_data'],true));
+        }elseif($this->data['brand_id'] == "合利宝GM"){
+            $res = http_post_data($this->url."/notice/helibao2/payment", json_decode($this->data['Raw_data'],true));
+        }else{
+            echo "跳过!";
+            $status->send_status='3';
+            $status->save();
+            return;
+        }
+        if($res['0']!="200"){
+            $this->fail();
+            $status->send_status='4';
+            echo "失败!";
+        }else{
+            $status->send_status='2';
+            echo "成功!";
+        }
+        $status->save();
+
+
+
+
     }
 }
