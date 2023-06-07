23eedb8f33fe60c1420140e7c9a482d938833bb0
zcc <1452073959@qq.com>
2023-06-07 15:14:27 +0800

    1

--- app/Models/NzfLog.php
+++ app/Models/NzfLog.php
@@ -10,5 +10,11 @@ class NzfLog extends Model
 {
 	use HasDateTimeFormatter;
     protected $table = 'nzf_log';
-    
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+    public $fillable = [
+        'url', 'method', 'params', 'domain', 'time','response_code','response_content'
+    ];
 }
--- app/Models/Td2Log.php
+++ app/Models/Td2Log.php
@@ -10,5 +10,11 @@ class Td2Log extends Model
 {
 	use HasDateTimeFormatter;
     protected $table = 'td2_log';
-    
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+    public $fillable = [
+        'url', 'method', 'params', 'domain', 'time','response_code','response_content'
+    ];
 }
